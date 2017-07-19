d1a68079b294caf9fec8797de4823d8a9763ef9d
yfern328 <yfernan2@u.rochester.edu>
2017-07-19 18:26:09 -0400

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,17 @@ const robots = [
   { name: 'Ratchet', alliance: null }
 ];
 
+const sortedRobots = robots.map(robot => {
+  let new_robot = Object.assign({}, robot);
+    if(knownDecepticons.includes(new_robot.name)){
+      new_robot.alliance = "decepticon"
+    }
+    else{
+      new_robot.alliance = "autobot"
+    };
+    return new_robot;
+});
+
 const zebraStripes = [
   { width: 9.12, color: null },
   { width: 5.71, color: null },
@@ -21,3 +32,14 @@ const zebraStripes = [
   { width: 0.59, color: null },
   { width: 7.31, color: null }
 ];
+
+const coloredZebraStripes = zebraStripes.map((zebra, idx) => {
+  let new_zebra = Object.assign({}, zebra);
+  if(idx % 2 === 0){
+    new_zebra.color = "black"
+  }
+  else{
+    new_zebra.color = "white"
+  };
+  return new_zebra;
+})
